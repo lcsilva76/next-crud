aae26ad49c1bed8fb30e10963788e151057e2ab3
lcsilva76 <lsilva@fiap.com.br>
2024-09-23 21:56:53 -0300

    first commit

---
+++ src/app/api/blogs/[id]/route.ts
@@ -0,0 +1,14 @@
+export const GET = async (req:Request)=>{
+    console.log('GET com parâmetro');
+    
+}
+
+export const PUT = async (req:Request)=>{
+    console.log('PUT com parâmetro');
+    
+}
+
+export const DELETE = async (req:Request)=>{
+    console.log('DELETE com parâmetro');
+    
+}
---
+++ src/app/api/blogs/route.ts
@@ -0,0 +1,12 @@
+export const GET = async (req:Request,res:Response)=>{
+    console.log('GET REQUEST');
+    
+}
+
+export const POST = async (req:Request,res:Response)=>{
+    console.log('POST REQUEST');
+    
+}
+
+//https://www.youtube.com/watch?v=O-NGENb6LNg
+//13:48
